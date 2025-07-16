cf7c2a19f6c12adf40dc847da4869615e9e1c728
rohit-002370 <rohitbangar1820@gmail.com>
2025-07-16 12:05:37 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+#Taking tow numbers as input from the user
+num1=int(input("Enter the first number: "))
+num2=int(input("enter the second number: "))
+#performing the basic mathematical on these two numbers:
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+#displaying the result of each operation
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
---
+++ Task2.py
@@ -0,0 +1,7 @@
+#taking first name and last name as input
+firstname =input("Enter your first name: ")
+lastname =input("Enter your last name: ")
+#Concatenates the first and last name into full name
+fullname =(firstname + " " + lastname)
+#result
+print(f"hello,{fullname}! welcome to the python program.")
